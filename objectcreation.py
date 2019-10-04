503b33cc7d8b9a84f5986b27f0e35270e1f23757
Daniel Gil <42021676+srd4@users.noreply.github.com>
2019-10-04 14:09:35 -0500

    Update objectcreation.py

--- objectcreation.py
+++ objectcreation.py
@@ -1,14 +1,8 @@
-class pom:
-    def __init__(self,t):
-        self.time = t[0]
-        self.date = t[1]
-        self.description = t[2]
-        self.code = t[3]
-        self.duration = t[4]
+import auxFunctions as af
 
 def getFile():
     """Returns list of all lines of poms in history file."""
-    with open("tomato_history.csv","r") as file:
+    with open("tomato_history.csv","r", encoding="utf-8") as file:
         data = file.readlines()
         t = []
 
@@ -17,15 +11,39 @@ def getFile():
 
         return t
 
-def createObjects():
-    """Crates pom objects."""
-    t = getFile()
-    obs = []
+class pom:
+    """Pomodoro objects, initiates with its data."""
+    def __init__(self,t):
+        self.time = af.cleanString(t[0])
+        self.date = af.cleanString(t[1])
+        self.description = af.cleanString(t[2])
+        self.code = af.cleanString(t[3])
+        self.duration = af.cleanString(t[4])
 
-    for i in t:
-        obs.append(pom(i))
+class dataBase:
+    """Created once. Used to apply methods to all poms."""
+    def __init__(self):
+        self.obs = self.create()
 
-    return obs
+    def getBy(self, attribute, required):
+        """Returns all objects that match atribute and requirement.
+takes: string, string
+returns: list."""
+        t = []
+        for i in self.obs:
+            if getattr(i,attribute) == required:
+                t.append(i)
+        return t
+            
+    def create(self):
+        """Crates pom objects."""
+        t = getFile()
+        obs = []
 
-global OBJECTS
-OBJECTS = createObjects()
+        for i in t:
+            obs.append(pom(i))
+
+        return obs
+
+dbase = dataBase()
+OBJECTS = dbase.obs
