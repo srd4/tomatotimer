6b75102b826a34da361b58b11089dc62b724120a
Daniel Gil <42021676+srd4@users.noreply.github.com>
2019-10-04 14:09:40 -0500

    Update getData.py

--- getData.py
+++ getData.py
@@ -1,5 +1,6 @@
 import objectcreation as ob
 import dateparsing as dp
+import datetime as dt
 import time
 
 
@@ -10,22 +11,24 @@ def takeSec(x):
 def totalTime():
     """returns total time doing all pomodoros of all kinds
 returns int"""
-    OBJECTS = ob.createObjects()
+    dbase = ob.dataBase()
+    OBJECTS = dbase.obs
+
     t = []
     for i in OBJECTS:
         t.append(int(i.duration))
 
     return sum(t)
 
-
 def sinceFirst():
     """returns days since first pomodoro"""
-    OBJECTS = ob.createObjects()
+    dbase = ob.dataBase()
+    OBJECTS = dbase.obs
     return int(dp.toSingleDay(time.localtime()))-int(dp.toSingleDay(dp.toStruct(OBJECTS[0].date)))
 
 
 def inSecs(string):
-    """turns time expressed in string as hh:nn:ss on int representing secods"""
+    """Returns time expressed in string hh:nn:ss as int representing secods"""
     hours = int(string[0:2])
     minutes = int(string[3:5])
     secs = int(string[6:])
@@ -33,7 +36,7 @@ def inSecs(string):
 
 
 def timeFormat(seconds):
-    """turns secs in nice format
+    """Turns secs into nice format.
 takes: int
 returns: string"""
     days = int(seconds/60/60/24)
@@ -44,32 +47,66 @@ returns: string"""
 
     return formated
 
+def pomsToday(date=time.strftime("%d/%m/%Y")):
+    """Dictionary of poms done today."""
+    t = ob.dataBase().getBy("date",date)
+
+    d = dict()
+
+    for i in t:
+        d[i.code] = d.get(i.code,0)+int(i.duration)
+        
+    return d
+
+def getWeekPoms(start=time.strftime("%d/%m/%Y")):
+    """Returns histogram of everything done in last seven days
+(including today)."""
+    dates = []
+    d = dict()
+    for i in range(0,7):
+        change = dt.datetime.strptime(start, "%d/%m/%Y") + dt.timedelta(days=-i)
+        dates.append(pomsToday(change.strftime("%d/%m/%Y")))
+
+    for i in dates:
+        for key in i:
+            d[key] = d.get(key,0)+i[key]
+
+    return d
+
+def weekAverage(d=getWeekPoms()):
+    """Returns average worked last seven days."""
+    return timeFormat(int((sum(d.values())*60)/7))[7:]
 
 def fromCode(code):
     """returns all pomodoros of certain code
 takes: string
 returns: list"""
-    OBJECTS = ob.createObjects()
+    dbase = ob.dataBase()
+    OBJECTS = dbase.obs
     t = []
     for i in OBJECTS:
         if i.code == code:
             t.append(i)
             
     return t
+    
+
 
 def getPomTime(code):
-    """counts time spent in a pomodoro of a given code
+    """Counts time spent in a pomodoro of a given code
 takes: string
-returns: int (minutes)"""
-    poms = fromCode(code)
-    t = 0
-    for i in poms:
-        t += int(i.duration)
+returns: int (minutes)."""
+
+    t = ob.dataBase().getBy("code",code)
+    
+    count = 0
+    for i in t:
+        count += int(i.duration)
         
-    return t
+    return count
 
 def longAgo(code):
-    """difference between date and today in days format"""
+    """Difference between date and today in days format."""
     date = fromCode(code)[-1].date
     return int(dp.toSingleDay(time.localtime()))-int(dp.toSingleDay(dp.toStruct(date)))
 
@@ -81,7 +118,9 @@ def pomLastTime(code):
 def allCodes():
     """returns list of strings, each one is a code, they're sorted for
 appearences"""
-    OBJECTS = ob.createObjects()
+    dbase = ob.dataBase()
+    OBJECTS = dbase.obs
+    
     d = dict()
     tuplesList = []
     sortedCodes = []
@@ -99,7 +138,6 @@ appearences"""
         sortedCodes.append(str(i[0]))
 
     return sortedCodes
-        
 
 def inSecs(string):
     """turns time expressed in string as hh:nn:ss on int representing secods"""
@@ -107,12 +145,4 @@ def inSecs(string):
     minutes = int(string[3:5])
     secs = int(string[6:])
     return (hours*60*60)+(minutes*60)+secs
-
-
-
-
-
-
-
-
 
